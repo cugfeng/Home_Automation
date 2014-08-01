3e286e9f17bc0071f1927c19964621669864e8a6
cugfeng@gmail.com <cugfeng@gmail.com>
2014-08-01 08:54:07 +0800

    Refine code: tab to 4 spaces

--- temperature/script/auto.sh
+++ temperature/script/auto.sh
@@ -5,19 +5,19 @@ sudo chmod 0666 $FLAG
 
 status="unknown"
 while [ 1 ]; do
-	hour=`date +%H`
+    hour=`date +%H`
     if [ $hour -eq 6 ] && [ $status != "disabled" ]; then
         echo "Time is 6:00, disable automode"
         echo 0 > $FLAG
-		status="disabled"
-		echo "Turn off AC"
-		irsend SEND_ONCE AC OFF
-	elif [ $hour -eq 22 ] && [ $status != "enabled" ]; then
-		echo "Time is 22:00, enable automode"
-		echo 1 > $FLAG
-		status="enabled"
+        status="disabled"
+        echo "Turn off AC"
+        irsend SEND_ONCE AC OFF
+    elif [ $hour -eq 22 ] && [ $status != "enabled" ]; then
+        echo "Time is 22:00, enable automode"
+        echo 1 > $FLAG
+        status="enabled"
     fi
 
-	sleep 60
+    sleep 60
 done
 
