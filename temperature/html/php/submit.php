d31ff192acf7f9e9b0db87c74c856204a9d4c205
cugfeng <cugfeng@gmail.com>
2014-08-09 14:14:03 +0800

    Add start time and stop time for automode

--- temperature/html/php/submit.php
+++ temperature/html/php/submit.php
@@ -4,7 +4,7 @@ $TEMP_SETTING_DIR  = "/tmp/temperature";
 $TEMP_SETTING_JSON = "/tmp/temperature/setting.json";
 
 function _post($var) {
-    if (!empty($_POST[$var])) {
+    if (isset($_POST[$var])) {
         return $_POST[$var];
     } else {
         return null;
@@ -16,28 +16,36 @@ if (!file_exists($TEMP_SETTING_DIR)) {
     chmod($TEMP_SETTING_DIR, 0777);
 }
 
-$automode = _post("automode");
-if ($automode == null) {
-    $automode = 0;
+$automode    = _post("automode");
+$from_hour   = _post("from_hour");
+$from_minute = _post("from_minute");
+$from_ampm   = _post("from_ampm");
+$to_hour     = _post("to_hour");
+$to_minute   = _post("to_minute");
+$to_ampm     = _post("to_ampm");
+$target      = _post("target");
+$tolerance   = _post("tolerance");
+
+if ($automode == 1) {
+$data = "{
+    \"automode\": $automode,
+    \"from_hour\": $from_hour,
+    \"from_minute\": $from_minute,
+    \"from_ampm\": \"$from_ampm\",
+    \"to_hour\": $to_hour,
+    \"to_minute\": $to_minute,
+    \"to_ampm\": \"$to_ampm\",
+    \"target\": $target,
+    \"tolerance\": $tolerance
+}";
+} else {
+    $data = "{ \"automode\": $automode }";
 }
 
-$target = _post("target");
-if ($target == null) {
-    $target = -1;
-}
-
-$tolerance = _post("tolerance");
-if ($tolerance == null) {
-    $tolerance = -1;
-}
-
-$data = "{ \"automode\": $automode, \"target\": $target, \"tolerance\": $tolerance }";
 $fp = fopen($TEMP_SETTING_JSON, "w");
 fwrite($fp, $data);
 fclose($fp);
 
-echo "Automode = $automode\n";
-echo "Target = $target\n";
-echo "Tolerance = $tolerance\n";
+echo "$data"
 
 ?>
